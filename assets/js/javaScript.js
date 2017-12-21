d18ceae0d428dbe5e0e5802166554c9d3c299cb2
Joshua Loudermilk <jdloude@gmail.com>
2017-12-20 22:40:04 -0700

    test1

---
+++ assets/js/javaScript.js
@@ -0,0 +1,25 @@
+// on click event that loads in text of clicked link from array to push to the api
+function liftStatus() {
+
+	$('.resorts').empty();
+
+	let resort = "breck";
+
+	let queryURL = "http://liftie.info/?resorts=" + resort;
+
+
+	$.ajax({
+
+        url: queryURL,
+
+        method: "GET"
+
+        // when done loading the inforamtion call this function
+        }).done(function (response) {
+
+        	console.log(response);
+        });
+
+}
+
+liftStatus();
